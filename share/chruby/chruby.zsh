4bd268aa06011188cd223df503871c742d8d3d15
Josh McGee <josh.s.mcgee@gmail.com>
2013-08-13 06:08:34 -0700

    clean folds

--- share/chruby/chruby.zsh
+++ share/chruby/chruby.zsh
@@ -1,21 +1,28 @@
 function chruby_set_preexec {
   typeset hook=()
+  # {{{ hook removal
   if [[ $1 == -r ]]; then
     precmd_functions=(${precmd_functions//chruby_auto})
     preexec_functions=(${preexec_functions//chruby_auto})
     return
   fi
+  # }}}
+  # {{{ hook detect && remove
   if {
     [[ $precmd_functions == *chruby_auto* ]] \
     || [[ $preexec_functions == *chruby_auto* ]]
   }; then
     chruby_set_preexec -r
   fi
+  # }}}
+  # {{{ append hook
   if [[ -o interactive ]]; then
     precmd_functions+=(chruby_auto)
   else
     preexec_functions+=(chruby_auto)
   fi
+  # }}}
 }
 . /etc/profile.d/chruby.sh
 
+# vim: ft=zsh sts=2 sw=2 fdm=marker
