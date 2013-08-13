0dbe8ffb98609ab3b28231962f5b6fcfac86b3cf
Josh McGee <josh.s.mcgee@gmail.com>
2013-08-13 03:17:57 -0700

    finishing touches

--- share/chruby/chruby.sh
+++ share/chruby/chruby.sh
@@ -1,27 +1,27 @@
-chruby_version="0.3.6"
+chrubyversion="0.3.6"
 
 ## utility functions
-function chruby_color_set {
+function chruby_set_color {
   if [[ -t 2 ]]; then
     if tput setaf 0 >/dev/null 2>&1; then
-      chruby_coff=$(tput sgr0)
-      chruby_bold=$(tput bold)
-      chruby_red=${chruby_bold}$(tput setaf 1)
-      chruby_green=${chruby_bold}$(tput setaf 2)
-      chruby_yellow=${chruby_bold}$(tput setaf 3)
-      chruby_blue=${chruby_bold}$(tput setaf 4)
+      chrubycoff=$(tput sgr0)
+      chrubybold=$(tput bold)
+      chruby_set_red=${chrubybold}$(tput af 1)
+      chruby_set_green=${chrubybold}$(tput af 2)
+      chruby_set_yellow=${chrubybold}$(tput af 3)
+      chruby_set_blue=${chrubybold}$(tput af 4)
     else
-      chruby_coff="\e[1;0m"
-      chruby_bold="\e[1;1m"
-      chruby_red="${chruby_bold}\e[1;31m"
-      chruby_green="${chruby_bold}\e[1;32m"
-      chruby_yellow="${chruby_bold}\e[1;33m"
-      chruby_blue="${chruby_bold}\e[1;34m"
+      chrubycoff="\e[1;0m"
+      chrubybold="\e[1;1m"
+      chruby_red="${chrubybold}\e[1;31m"
+      chruby_green="${chrubybold}\e[1;32m"
+      chruby_yellow="${chrubybold}\e[1;33m"
+      chruby_blue="${chrubybold}\e[1;34m"
     fi
   fi
 }
 
-function chruby_env_path_clean {
+function chruby_clean_env_path {
   PATH=:$PATH:
   PATH=${PATH//:$RUBY_ROOT?bin:/:}
   PATH=${PATH//:$GEM_HOME?bin:/:}
@@ -30,7 +30,7 @@ function chruby_env_path_clean {
   PATH=${PATH%:}
 }
 
-function chruby_env_gempath_clean {
+function chruby_clean_env_gempath {
   GEM_PATH=:$GEM_PATH:
   GEM_PATH=${GEM_PATH//:$GEM_HOME:/:}
   GEM_PATH=${GEM_PATH//:$GEM_ROOT:/:}
@@ -38,10 +38,10 @@ function chruby_env_gempath_clean {
   GEM_PATH=${GEM_PATH%:}
 }
 
-function chruby_env_set {
-  typeset env new_ruby_root=$1 ruby_opt=${*:2}
-  chruby_env_path_clean
-  chruby_env_gempath_clean
+function chruby_set_env {
+  typeset env new_ruby_root=${1%/bin/*} ruby_opt=${*:2}
+  chruby_clean_env_path
+  chruby_clean_env_gempath
   RUBY_ROOT=${new_ruby_root:-$sys_ruby_root}
   RUBYOPT=${ruby_opt:-$RUBYOPT}
   while IFS= read -r env; do
@@ -69,21 +69,21 @@ EOR
   hash -r
 }
 
-function chruby_default_rubies_set {
+function chruby_set_default_rubies {
   typeset dir
   rubies=()
-  for dir in "$RUBY_PREFIX"/* "$HOME"/.rubies/*; do
+  for dir in "$HOME"/.rubies/*; do
     [[ -e $dir && -x $dir/bin/ruby ]] && rubies+=("$dir")
   done
 }
 
-function chruby_default_set {
+function chruby_set_default {
   sys_ruby_root=$(PATH=/usr/local/bin:/usr/bin:/bin command -v ruby)
   sys_ruby_root=${sys_ruby_root%/bin/*}
-  chruby_env_set "$sys_ruby_root"
+  chruby_set_env "$sys_rubyroot"
 }
 
-function chruby_auto {
+function chrubyauto {
   typeset ver dir=$PWD stop=${HOME%/*}
   [[ $dir == $stop* ]] || return
   until [[ $dir == $stop ]]; do
@@ -97,37 +97,38 @@ function chruby_auto {
 
 ## user functions
 function chruby {
-  typeset match msg colored optstring=:hV rb=${rubies[*]}
+  typeset o match colored optstring=:hV rb=${rubies[*]}
   [[ $1 == --* ]] && set -- "${1#-}" "${@:2}"
   if getopts $optstring o; then
     case $o in
       h)
-	printf '%s\n' "usage: chruby [RUBY|VERSION|system] [RUBY_OPTS]"
+	printf '%s\n' "usage: chruby [RUBY|VERSION|system] [RUBYOPTS]"
 	return
       ;;
       V)
-	printf '%s\n' "chruby version: $chruby_version"
+	printf '%s\n' "chruby version: $chrubyversion"
 	return
       ;;
     esac
+    # shift $((OPTIND-1))
   fi
   case $1 in
     '')
-      colored=${chruby_blue}*\ ${chruby_coff}
-      colored=${colored}${chruby_green}$RUBY_ROOT${chruby_off}
+      colored=${chruby_blue}*\ ${chrubycoff}
+      colored=${colored}${chruby_green}$RUBY_ROOT${chrubyoff}
       printf '%s\n' "${rubies[@]/#$RUBY_ROOT/$colored}"
     ;;
     system)
-      chruby_default_set
+      chruby_set_default
     ;;
     *)
       while ((rb-- >= 0)); do
 	[[ ${rubies[rb]} == *$1* ]] && { match=${rubies[rb]}; break; }
       done
       if [[ -n $match ]]; then
-	chruby_env_set "$match" "${@:2}"
+	chruby_set_env "$match" "${@:2}"
       else
-	printf '%s\n' "$msg"
+	printf '%s\n' "No ruby found for '$1'"
 	return 2
       fi
 
@@ -148,12 +149,12 @@ if (($#)); then
   done
 fi
 
-((enable_defaults)) && chruby_default_set
-((enable_rubies)) && chruby_default_rubies_set
-((enable_color)) && chruby_color_set
-((enable_auto)) && chruby_preexec_set
+((enable_defaults)) && chruby_set_default
+((enable_rubies)) && chruby_set_default_rubies
+((enable_color)) && chruby_set_color
+((enable_auto)) && chruby_set_preexec
 
 unset optstring o \
   enable_auto enable_color enable_defaults enable_rubies
-export GEM_HOME GEM_PATH GEMSKIP GEM_SKIP RUBY_ROOT RUBYOPT RUBY_PREFIX
+export GEM_HOME GEM_PATH GEMSKIP GEM_SKIP RUBY_ROOT RUBYOPT
 
