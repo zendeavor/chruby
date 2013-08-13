fcea3345703fa0af6423a5fe8cb532f7a05b0e59
Josh McGee <josh.s.mcgee@gmail.com>
2013-08-13 06:20:32 -0700

    Revert "clean folds"
    This reverts commit 4bd268aa06011188cd223df503871c742d8d3d15.

--- share/chruby/chruby.bash
+++ share/chruby/chruby.bash
@@ -1,7 +1,6 @@
 function chruby_set_preexec {
   typeset hook=()
   typeset trap=$(trap -p DEBUG)
-  # {{{ hook removal
   if [[ $1 == -r ]]; then
     PROMPT_COMMAND=${PROMPT_COMMAND//chruby_auto?}
     IFS=\' read -ra hook <<<"$trap"
@@ -9,32 +8,22 @@ function chruby_set_preexec {
     trap "${hook[@]//chruby_auto?(;)}" DEBUG
     return
   fi
-  # }}}
-  # {{{ hook detect && remove
   if {
       [[ $PROMPT_COMMAND == *chruby_auto* ]] \
 	|| [[ $trap == *chruby_auto* ]];
   }; then
     chruby_set_preexec -r
   fi
-  # }}}
-  # {{{1 hook add
-  # {{{2 interactive shells
   if [[ $- == *i* ]]; then
     IFS=\; read -ra hook <<<"$PROMPT_COMMAND"
     PROMPT_COMMAND="${hook[@]/%/;} chruby_auto"
     PROMPT_COMMAND="${PROMPT_COMMAND#;}"
-  # 2}}}
   else
-  # {{{2 non-interactive shells
     IFS=\' read -ra hook <<<"$trap"
     IFS=\; read -ra hook <<<"${hook[@]:1:${#hook[@]}-2}"
     trap "${hook[@]/%/;} chruby_auto" DEBUG
-  # 2}}}
   fi
-  # 1}}}
 }
 shopt -s extglob
 . /etc/profile.d/chruby.sh
 
-# vim: ft=sh sts=2 sw=2 fdm=marker
